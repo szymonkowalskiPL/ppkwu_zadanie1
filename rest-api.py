b36c10f6b936827cbd772d3a24873badc0392de2
szkowalski <222477@edu.p.lodz.pl>
2021-10-21 13:30:18 +0200

    md5 hash added

--- rest-api.py
+++ rest-api.py
@@ -1,11 +1,14 @@
 from flask import Flask, request, jsonify
+import hashlib
 
 app = Flask(__name__)
 
 @app.route('/calculate',  methods=['GET', 'POST'])
 def calculateChecksum():
     string = request.args.get('string')
-    
-    return jsonify(string)
+    md5hash = hashlib.md5(string.encode('utf-8')).hexdigest()
+    return jsonify(
+        md5=md5hash
+    )
 
 app.run()
