4ae2292caec824f422986adb8bf84bdd58654176
szkowalski <222477@edu.p.lodz.pl>
2021-10-21 13:12:27 +0200

    send request with arg fixed

--- rest-api.py
+++ rest-api.py
@@ -5,6 +5,7 @@ app = Flask(__name__)
 @app.route('/calculate',  methods=['GET', 'POST'])
 def calculateChecksum():
     string = request.args.get('string')
-    return 'your string to hash: '.string
+    
+    return 'your string to hash: '+ string
 
 app.run()
