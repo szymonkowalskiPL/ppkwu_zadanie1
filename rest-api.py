c014d3fd2b4b845680ce98cfbca52fd346b5aa17
szkowalski <222477@edu.p.lodz.pl>
2021-10-21 13:54:27 +0200

    sha1 and sha256 hashes added

--- rest-api.py
+++ rest-api.py
@@ -8,14 +8,20 @@ app = Flask(__name__)
 @app.route('/calculate',  methods=['GET', 'POST'])
 def calculateChecksum():
     string = request.args.get('string')
+    
     md5hash = hashlib.md5(string.encode('utf-8')).hexdigest()
 
     crc32hash = binascii.crc32(string.encode('utf8'))
     
+    sha1hash = hashlib.sha1(string.encode('utf-8')).hexdigest()
+    
+    sha256hash = hashlib.sha256(string.encode('utf-8')).hexdigest()
     
     return jsonify(
         md5=md5hash,
         crc32=crc32hash,
+        sha1=sha1hash,
+        sha256=sha256hash,
     )
 
 app.run()
