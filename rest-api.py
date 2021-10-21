615e13193b38d672691a8f06c7e7ff47431aa8ea
szkowalski <222477@edu.p.lodz.pl>
2021-10-21 12:53:46 +0200

    first test endpoint created

--- rest-api.py
+++ rest-api.py
@@ -1,5 +1,9 @@
-from flask import flask
+from flask import Flask
 
 app = Flask(__name__)
+
+@app.route('/calculate',  methods=['GET', 'POST'])
+def calculateChecksum():
+    return 'test'
 
 app.run()
