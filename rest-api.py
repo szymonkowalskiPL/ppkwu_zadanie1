28f9db8495809a6a8a78981db89a8d1d46e1cd2d
szkowalski <222477@edu.p.lodz.pl>
2021-10-21 13:09:37 +0200

    send request with arg failed

--- rest-api.py
+++ rest-api.py
@@ -1,9 +1,10 @@
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
 @app.route('/calculate',  methods=['GET', 'POST'])
 def calculateChecksum():
-    return 'test'
+    string = request.args.get('string')
+    return 'your string to hash: '.string
 
 app.run()
